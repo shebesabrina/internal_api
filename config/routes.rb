997e54bce2116f7244c8b34c219947b10fe3298f
Sabrina <sabrinarob@gmail.com>
2019-02-06 11:47:17 -0700

    Implements order controller with show and idex routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'orders/index'
+      get 'orders/show'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       resources :items, except: [:new, :edit]
     end
   end
